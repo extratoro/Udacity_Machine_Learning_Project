bb8c117834c21c62f43bffdf34c339e09a666941
extratoro <cault10@yahoo.fr>
2015-07-28 22:57:09 +0200

    New submission
    new classifier, cleaner code and new answers

--- poi_id.py
+++ poi_id.py
@@ -2,6 +2,8 @@
 
 import sys
 import pickle
+import pprint
+
 
 sys.path.append("../tools/")
 
@@ -11,11 +13,22 @@ from tester import test_classifier, dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', 'deferral_payments', 'restricted_stock_deferred', 
-                 'deferred_income', 'director_fees', 'exercised_stock_options', 
-                 'shared_receipt_with_poi', 'deferred_stock_ratio']
+features_list = ['poi', 
+                 'salary', 
+                 'bonus', 
+                 'deferred_income', 
+                 'expenses', 
+                 'exercised_stock_options', 
+                 'other', 
+                 'long_term_incentive', 
+                 'restricted_stock', 
+                 'from_poi_to_this_person', 
+                 'from_this_person_to_poi',  
+                 'shared_receipt_with_poi',
+                 'deferral_ratio', 
+                 'sent_to_poi_ratio']
 
- # You will need to use more features
+
 
 ### Load the dictionary containing the dataset
 data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
@@ -38,11 +51,9 @@ for person in person_null:
 
 print 'number of people in the dataset: '+ str(len(my_dataset.keys()))
 print 'Removed data point: ' + str(person_null)
-
-
 print features_list
 
-#Adding new features
+### Adding new features
 for person in my_dataset:
 
     #deferal ratio, check for 0 denominator and NaN value
@@ -72,26 +83,48 @@ data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.decomposition import KernelPCA
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import StratifiedKFold
 from sklearn.feature_selection import SelectKBest
 
-clf = RandomForestClassifier(n_estimators=100, min_samples_split=4, 
-                             bootstrap=False, criterion='entropy',
-                             class_weight='subsample', max_depth=2)
+### Setting up initial selection, classifier and the pipeline
+selection = SelectKBest()
+rfc = RandomForestClassifier()
+pipeline = Pipeline([('features', selection),
+                     ('classifier', rfc)])
 
-#clf = Pipeline([('feat', FeatureUnion([('pca', KernelPCA(kernel='rbf', 
-#                                                         n_components=2)),
-#                                        ('kbest', SelectKBest(k=7))])),
-#                ('clf',rand)])
+### Dict of gridsearch params to go through
+parameters = { 'features__k': [5, 7, 10, 'all'],
+                'classifier__n_estimators': [50, 100, 200],
+               'classifier__min_samples_split': [4, 6, 8], 
+                'classifier__criterion': ['entropy', 'gini'],
+                'classifier__class_weight': ['subsample', 'auto', None],
+                'classifier__max_depth': [2, 4, 6],
+                'classifier__warm_start': [False, True]
+                }
 
 
-test_classifier(clf, my_dataset, features_list)
-print '\n forest feature importance:\n'
-print clf.feature_importances_
+### Gridsearch and fit to get best params
+clf = GridSearchCV(pipeline, parameters, 
+                   cv=StratifiedKFold(labels,n_folds=5),
+                   scoring='recall')
+                   
+clf.fit(features, labels)
 
+print '\nbest params\n'
+pprint.pprint(clf.best_params_)  
+print '\nbest score\n'                     
+pprint.pprint(clf.best_score_)
+print '\nkbest features\n'
+pprint.pprint(clf.best_estimator_.named_steps['features'].scores_)
+print '\nrfc features importance\n'
+pprint.pprint(clf.best_estimator_.named_steps['classifier'].feature_importances_)
+
+
+test_classifier(clf.best_estimator_, my_dataset, features_list)
 
 ### Dump your classifier, dataset, and features_list so 
 ### anyone can run/check your results.
 
-dump_classifier_and_data(clf, my_dataset, features_list)
+dump_classifier_and_data(clf.best_estimator_, my_dataset, features_list)
